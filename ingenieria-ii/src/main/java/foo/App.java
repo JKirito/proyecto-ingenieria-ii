518d04a544c623924c6c5a6552258d73768e3d94
j.godoy277@gmail.com <j.godoy277@gmail.com@4f399e54-ad59-8ecc-b7a7-fbb065668147>
2013-10-21 01:48:09 +0000

    modificado el método

--- ingenieria-ii/src/main/java/foo/App.java
+++ ingenieria-ii/src/main/java/foo/App.java
@@ -2,21 +2,20 @@ package foo;
 
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-    
-    public boolean isVerdad(){
-    	return true;
-    }
-    
-    public int devolverDoble(Integer n){
-    	return n*2-5;
-    }
-    
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+	public boolean isVerdad() {
+		return true;
+	}
+
+	public int devolverDoble(Integer n) {
+		return n * 2;
+	}
+
 }
